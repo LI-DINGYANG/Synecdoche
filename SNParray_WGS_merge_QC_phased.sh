bcb377e5a07ad713999d6852c143fd03a21f41ad
LIDINGYANG <43173290+LI-DINGYANG@users.noreply.github.com>
2025-05-30 12:13:17 +0800

    Update SNParray_WGS_merge_QC_phased.sh

--- SNParray_WGS_merge_QC_phased.sh
+++ SNParray_WGS_merge_QC_phased.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-#BSUB -J QC               # Job name
-#BSUB -o QC.%J.out        # Standard output file
-#BSUB -e QC.%J.err        # Standard error file
-#BSUB -q q6240            # Queue name
-#BSUB -n 30               # Number of CPU cores
 
 bcftools norm -f /path/to/hg38.fa \
   -m -any \
@@ -17,37 +12,37 @@ awk 'BEGIN {print "#IID SEX"} {print $2, $5}' Chinese.hg38.chrXY.sex.fam > Chine
 plink2 --vcf Chinese.norm.vcf.gz --split-par hg38 --update-sex Chinese.hg38.sex.info.txt --make-bed --allow-extra-chr 0 \
 --vcf-half-call m --out Chinese.hg38
 
-plink2 --bfile ASA.JN --exclude ASAJN.duplicated.txt --recode vcf --output-chr chrMT --out JN
-plink2 --bfile ASA.GZ --recode vcf --output-chr chrMT --out GZ
+plink2 --bfile ASA.HK1 --exclude ASAHK1.duplicated.txt --recode vcf --output-chr chrMT --out JN
+plink2 --bfile ASA.HK2 --recode vcf --output-chr chrMT --out HK2
 
-bcftools +fixref JN.vcf -Oz -o JN.fixed.vcf.gz \
+bcftools +fixref HK1.vcf -Oz -o HK1.fixed.vcf.gz \
 -- -f /path/to/hg38.fasta -m top
 
-bcftools +fixref GZ.vcf -Oz -o GZ.fixed.vcf.gz \
+bcftools +fixref HK2.vcf -Oz -o HK2.fixed.vcf.gz \
 -- -f /path/to/hg38.fasta -m top
 
-plink --vcf GZ.fixed.vcf.gz --make-bed --keep-allele-order --double-id --out GZ.fixed
-plink --vcf JN.fixed.vcf.gz --make-bed --keep-allele-order --double-id --out JN.fixed
+plink --vcf HK2.fixed.vcf.gz --make-bed --keep-allele-order --double-id --out HK2.fixed
+plink --vcf HK1.fixed.vcf.gz --make-bed --keep-allele-order --double-id --out HK1.fixed
 
 awk '{$2 = "chr"$1":"$4":"$6":"$5; print}' Chinese.hg38.bim > temp.txt && rm Chinese.hg38.bim && mv temp.txt Chinese.hg38.bim
 awk '{$2 = $1":"$4":"$6":"$5; print}' Thai_chip_data.chr6.fixed.bim > temp.txt && rm Thai_chip_data.chr6.fixed.bim && mv temp.txt Thai_chip_data.chr6.fixed.bim
 
 awk '{print $2}' Chinese.hg38.bim | sort > Chinese.hg38_id.txt
-awk '{print $2}' JN.fixed.bim | sort > JN.fixed_id.txt
-awk '{print $2}' GZ.fixed.bim | sort > GZ.fixed_id.txt
+awk '{print $2}' HK1.fixed.bim | sort > HK1.fixed_id.txt
+awk '{print $2}' HK2.fixed.bim | sort > HK2.fixed_id.txt
 
-comm -12 JN.fixed_id.txt GZ.fixed_id.txt > intersection.txt
+comm -12 HK1.fixed_id.txt HK2.fixed_id.txt > intersection.txt
 comm -12 intersection.txt Chinese.hg38_id.txt > intersection2.txt
 
-plink --bfile GZ.fixed --extract intersection2.txt --keep-allele-order --recode vcf --output-chr chrMT --out GZ.samesnps
-plink --bfile JN.fixed --extract intersection2.txt --keep-allele-order --recode vcf --output-chr chrMT --out JN.samesnps
+plink --bfile HK2.fixed --extract intersection2.txt --keep-allele-order --recode vcf --output-chr chrMT --out HK2.samesnps
+plink --bfile HK1.fixed --extract intersection2.txt --keep-allele-order --recode vcf --output-chr chrMT --out HK1.samesnps
 plink2 --bfile Chinese.hg38 --extract intersection2.txt --recode vcf --output-chr chrMT --out Chinese.samesnps
 
-bgzip -@ 30 GZ.samesnps.vcf && tabix GZ.samesnps.vcf.gz
-bgzip -@ 30 JN.samesnps.vcf && tabix JN.samesnps.vcf.gz
+bgzip -@ 30 HK2.samesnps.vcf && tabix HK2.samesnps.vcf.gz
+bgzip -@ 30 HK1.samesnps.vcf && tabix HK1.samesnps.vcf.gz
 bgzip -@ 30 Chinese.samesnps.vcf && tabix Chinese.samesnps.vcf.gz
 
-bcftools merge JN.samesnps.vcf.gz GZ.samesnps.vcf.gz Chinese.samesnps.vcf.gz -Oz -o ASA.Chinese.vcf.gz
+bcftools merge HK1.samesnps.vcf.gz HK2.samesnps.vcf.gz Chinese.samesnps.vcf.gz -Oz -o ASA.Chinese.vcf.gz
 
 plink --vcf ASA.Chinese.vcf.gz --keep-allele-order --make-bed --out ASA.Chinese --double-id
 
@@ -91,3 +86,4 @@ do
 --geneticMapFile=/path/to/genetic_map_hg38_withX.txt.gz --numThreads=5 --outPrefix ASA.Chinese.chr${chr}.phased \
 --vcfOutFormat z &
 done
+
